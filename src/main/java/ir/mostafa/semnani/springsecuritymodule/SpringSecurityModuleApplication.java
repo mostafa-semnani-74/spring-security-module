5096ce4b57510415ff493aa79d3e391255108d9d
mostafa-semnani-74 <mostafa.semnani.74@gmail.com>
2024-03-08 12:52:11 +0330

    init spring security module project

---
+++ src/main/java/ir/mostafa/semnani/springsecuritymodule/SpringSecurityModuleApplication.java
@@ -0,0 +1,13 @@
+package ir.mostafa.semnani.springsecuritymodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityModuleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ir/mostafa/semnani/springsecuritymodule/SpringSecurityModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.mostafa.semnani.springsecuritymodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
