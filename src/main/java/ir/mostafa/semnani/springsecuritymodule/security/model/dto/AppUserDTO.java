0615ad01aca8f4f67fade83c20a8bed22f54ae92
mostafa-semnani-74 <mostafa.semnani.74@gmail.com>
2024-09-03 11:15:38 +0330

    add password regex for validation
    - min 12 char
    - include upper and lower char
    - include special char
    
    add new server port
    change id of admin and user in init sql

--- src/main/java/ir/mostafa/semnani/springsecuritymodule/security/model/dto/AppUserDTO.java
+++ src/main/java/ir/mostafa/semnani/springsecuritymodule/security/model/dto/AppUserDTO.java
@@ -1,6 +1,7 @@
 package ir.mostafa.semnani.springsecuritymodule.security.model.dto;
 
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,5 +16,7 @@ public class AppUserDTO {
     @NotNull
     private String username;
     @NotNull
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{12,}$",
+    message = "password must be at least 12 character with lower and upper and number and special character")
     private String password;
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,8 @@
 -- only for test
 
 -- create users
-INSERT INTO tbl_app_user (id, password, username) VALUES (1, '$2a$10$FnO4w5MjGvd79oYcsPxBGup7nBrtLGguDqyDJoH5xTBNqMWN2DO/i', 'admin');
-INSERT INTO tbl_app_user (id, password, username) VALUES (2, '$2a$10$Z19r4n1.GLjGDafxqSfGh.pLZpbIcsGu9r3nieZZ8MBY4N6f6VCFC', 'user');
+INSERT INTO tbl_app_user (id, password, username) VALUES (-1, '$2a$10$FnO4w5MjGvd79oYcsPxBGup7nBrtLGguDqyDJoH5xTBNqMWN2DO/i', 'admin');
+INSERT INTO tbl_app_user (id, password, username) VALUES (-2, '$2a$10$Z19r4n1.GLjGDafxqSfGh.pLZpbIcsGu9r3nieZZ8MBY4N6f6VCFC', 'user');
 
 -- create roles
 INSERT INTO tbl_app_role (id, name) VALUES (1, 'ROLE_ADMIN');
@@ -18,5 +18,5 @@ INSERT INTO roles_permissions (app_permission_id, app_role_id) VALUES (1, 2);
 INSERT INTO roles_permissions (app_permission_id, app_role_id) VALUES (2, 1);
 
 -- join user to roles
-INSERT INTO users_roles (app_role_id, app_user_id) VALUES (1, 1);
-INSERT INTO users_roles (app_role_id, app_user_id) VALUES (2, 2);
+INSERT INTO users_roles (app_role_id, app_user_id) VALUES (1, -1);
+INSERT INTO users_roles (app_role_id, app_user_id) VALUES (2, -2);
